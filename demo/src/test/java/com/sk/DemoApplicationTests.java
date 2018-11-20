1822d8eb92e1a8549c1eef08b60cb0e9dcdf87d2
Administrator <Administrator@SKCC18D00113.SKCC.NET>
2018-11-20 17:01:41 +0900

    Spring Boot sample

--- demo/src/test/java/com/sk/DemoApplicationTests.java
+++ demo/src/test/java/com/sk/DemoApplicationTests.java
@@ -1,14 +1,31 @@
 package com.sk;
 
+import javax.transaction.Transactional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.sk.service.AccountService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
+@ContextConfiguration(loader=SpringBootContextLoader.class, classes=DemoApplication.class)
+@WebAppConfiguration
+@Transactional
 public class DemoApplicationTests {
-
+	@Autowired
+	WebApplicationContext wac;
+	@Autowired
+	AccountService service;
+	
+	
 	@Test
 	public void contextLoads() {
 	}
